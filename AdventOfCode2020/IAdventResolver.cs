a5be78f7fd7ebf265599067b4c818943139228dc
Henrique Dal Bello <henrique.d.bello@gmail.com>
2020-12-31 13:43:28 -0300

    Initial commit with 2020 day 1 solution

---
+++ AdventOfCode2020/IAdventResolver.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2020
+{
+    public interface IAdventResolver
+    {
+        object ResolvePartOne();
+        object ResolvePartTwo();
+    }
+}
---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode2020.Day1;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunDay1();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+
+        static void RunDay1()
+        {
+            var reader = new StreamReader("./Day1/Input.txt");
+            var input = reader.ReadToEnd().Split("\r\n").Select(x => int.Parse(x));
+
+            var resolver = new Day1Resolver(input);
+            var result = resolver.ResolvePartOne();
+            Console.WriteLine($"[{nameof(Day1Resolver)} - Part One] => Result: {result}");
+
+            result = resolver.ResolvePartTwo();
+            Console.WriteLine($"[{nameof(Day1Resolver)} - Part Two] => Result: {result}");
+        }
+    }
+}
