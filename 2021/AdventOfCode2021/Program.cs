014db8ca83f9d5009a61953beef86b8905193b1a
Henrique Dal Bello <henrique.d.bello@gmail.com>
2023-09-20 20:04:12 -0300

    wip 2021, day 1

---
+++ 2021/AdventOfCode2021/Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCode2021.Day01;
+
+
+using var reader = new StreamReader($"./{typeof(Day01).Name}/Input.txt");
+var fileContent = reader.ReadToEnd();
+Console.WriteLine(new Day01(fileContent.Split("\r\n")).ResolvePartTwo());
+
